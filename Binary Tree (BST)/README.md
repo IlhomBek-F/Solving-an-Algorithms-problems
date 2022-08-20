9f3d948e8ce5eedb050e0d36c27c3a008d8f4869
User <futurewithme1221@gmail,com>
2022-08-20 16:08:32 +0500

    bst

---
+++ Binary Tree (BST)/README.md
@@ -0,0 +1,7 @@
+# About Binary Tree
+
+Binary search tree, as shown in its name, is an ordered tree data structure. Every parent nodes has at most two children, every node to the left of a parent node is always less than the parent and every node to the right of the parent node is always greater than the parent.
+
+if value is less than than the root then should be left subtree.
+
+If value is bigger than the root then should be right subtree.
---
+++ Binary Tree (BST)/bst.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+    while (current) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  find(value) {
+    if (this.root === null) return null;
+
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = current;
+      }
+    }
+
+    if (!found) return undefined;
+    return found;
+  }
+}
+
+const bst = new BinarySearchTree();
+
+bst.insert(1);
+bst.insert(2);
+bst.insert(3);
